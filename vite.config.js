fbed1c157a27318609892534f97807cb4af3dfb0
renoker <rodolfoulises.ramirez@gmail.com>
2024-04-25 11:03:40 -0600

    main.sass

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
                 'resources/css/app.css', 
                 'resources/js/app.js',
                 'resources/js/global.js',
-                'resources/js/home.js'
+                'resources/js/home.js',
+                'resources/sass/main.scss'
             ],
             refresh: true,
         }),
